0a78cd220c7e6639d044b4180a1ad83a44facc71
Anthony Biccum <atbiccum@gmail.com>
2021-09-23 16:10:51 -0700

    Added debugging for first and second text boxes.

--- calculatorGUI/MainWindow.xaml.cs
+++ calculatorGUI/MainWindow.xaml.cs
@@ -57,33 +57,48 @@ namespace calculatorGUI
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
             //Addition
-            double num1 = Convert.ToDouble(firstTextBox.Text);
-            double num2 = Convert.ToDouble(secondTextBox.Text);
-            double temp = num1 + num2;
+
+            //double num1 = Convert.ToDouble(firstTextBox.Text);
+            //double num2 = Convert.ToDouble(secondTextBox.Text);
+
+            string num1 = firstTextBox.Text;
+            string num2 = secondTextBox.Text;
+
+            double cleanNum1 = debugFirstBox(num1);
+            double cleanNum2 = debugSecondBox(num2);
+
+            double temp = cleanNum1 + cleanNum2;
             textAnswer.Text = Convert.ToString(temp);
         }
 
-        private void Button_Click_4(object sender, RoutedEventArgs e)
+        public double debugFirstBox(string number)
         {
-            //Equals
+            //Goal: Return a double that has been error checked and converted from string
 
+            double cleanNumber = 0;
+            if (!double.TryParse(number, out cleanNumber))                     //Try parse converts string to a double and saves under clean new name
+            {                                                                  //If a unvalid character will trigger warning
+                firstTextBox.Clear();
+                firstTextBox.Text = "Invalid Character: Try again";
+            }
+            return cleanNumber;
         }
-
-        private void operation1_TextChanged(object sender, TextChangedEventArgs e)
+        public double debugSecondBox(string number)
         {
-            //Operation 1 Input
+            //Goal: Return a double that has been error checked and converted from string
 
-        }
-
-        private void operation2_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            //Operation 2 Input 
-
-        }
-        private void answer_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            //Answer Output
-
+            double cleanNumber = 0;
+            if (!double.TryParse(number, out cleanNumber))                     //Try parse converts string to a double and saves under clean new name
+            {                                                                  //If a unvalid character will trigger warning
+                secondTextBox.Clear();
+                secondTextBox.Text = "Invalid Character: Try again";
+            }
+            while (cleanNumber == 0)                                         //Check if we are trying to divide by zero
+            {                                                           //Returns an error if trying to divide by zero
+                secondTextBox.Clear();
+                secondTextBox.Text = "Cannot Divide by 0: Try again";
+            }
+            return cleanNumber;
         }
     }
 }
