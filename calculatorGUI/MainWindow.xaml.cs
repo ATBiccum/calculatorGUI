bb68a78537a094910d04531c0168674c2b6e0d65
Anthony Biccum <atbiccum@gmail.com>
2021-09-23 16:28:42 -0700

    Added invalid character and divide by 0 debugging for all operators.

--- calculatorGUI/MainWindow.xaml.cs
+++ calculatorGUI/MainWindow.xaml.cs
@@ -28,77 +28,107 @@ namespace calculatorGUI
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //Division
-            double num1 = Convert.ToDouble(firstTextBox.Text);
-            double num2 = Convert.ToDouble(secondTextBox.Text);
-            double temp = num1 / num2;
+            string num1 = firstTextBox.Text;
+            string num2 = secondTextBox.Text;
+
+            //Checks for valid characters and if div by 0
+            double cleanNum1 = 0;
+            if (!double.TryParse(num1, out cleanNum1))            //Try parse converts string to a double and saves under clean new name
+            {                                                           //If a unvalid character will trigger warning
+                firstTextBox.Clear();
+                firstTextBox.Text = "Error: Invalid Char";
+            }
+
+            double cleanNum2 = 0;
+            if (!double.TryParse(num2, out cleanNum2))           //Will check if unvalid value and return warning
+            {
+                secondTextBox.Clear();
+                secondTextBox.Text = "Error: Invalid Char";
+            }
+
+            if (cleanNum2 == 0)                                         //Check if we are trying to divide by zero
+            {                                                           //Returns an error if trying to divide by zero
+                secondTextBox.Clear();
+                secondTextBox.Text = "Error: Div by 0";
+            }
+
+            double temp = cleanNum1 / cleanNum2;
             textAnswer.Text = Convert.ToString(temp);
-            
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             //Mupliplication
-            double num1 = Convert.ToDouble(firstTextBox.Text);
-            double num2 = Convert.ToDouble(secondTextBox.Text);
-            double temp = num1 * num2;
-            textAnswer.Text = Convert.ToString(temp);
+            string num1 = firstTextBox.Text;
+            string num2 = secondTextBox.Text;
 
+            //Checks for valid characters and if div by 0
+            double cleanNum1 = 0;
+            if (!double.TryParse(num1, out cleanNum1))            //Try parse converts string to a double and saves under clean new name
+            {                                                           //If a unvalid character will trigger warning
+                firstTextBox.Clear();
+                firstTextBox.Text = "Error: Invalid Char";
+            }
+
+            double cleanNum2 = 0;
+            if (!double.TryParse(num2, out cleanNum2))           //Will check if unvalid value and return warning
+            {
+                secondTextBox.Clear();
+                secondTextBox.Text = "Error: Invalid Char";
+            }
+
+            double temp = cleanNum1 * cleanNum2;
+            textAnswer.Text = Convert.ToString(temp);
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             //Subtraction
-            double num1 = Convert.ToDouble(firstTextBox.Text);
-            double num2 = Convert.ToDouble(secondTextBox.Text);
-            double temp = num1 - num2;
+            string num1 = firstTextBox.Text;
+            string num2 = secondTextBox.Text;
+
+            //Checks for valid characters and if div by 0
+            double cleanNum1 = 0;
+            if (!double.TryParse(num1, out cleanNum1))            //Try parse converts string to a double and saves under clean new name
+            {                                                           //If a unvalid character will trigger warning
+                firstTextBox.Clear();
+                firstTextBox.Text = "Error: Invalid Char";
+            }
+
+            double cleanNum2 = 0;
+            if (!double.TryParse(num2, out cleanNum2))           //Will check if unvalid value and return warning
+            {
+                secondTextBox.Clear();
+                secondTextBox.Text = "Error: Invalid Char";
+            }
+
+            double temp = cleanNum1 - cleanNum2;
             textAnswer.Text = Convert.ToString(temp);
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
             //Addition
-
-            //double num1 = Convert.ToDouble(firstTextBox.Text);
-            //double num2 = Convert.ToDouble(secondTextBox.Text);
-
             string num1 = firstTextBox.Text;
             string num2 = secondTextBox.Text;
 
-            double cleanNum1 = debugFirstBox(num1);
-            double cleanNum2 = debugSecondBox(num2);
+            //Checks for valid characters and if div by 0
+            double cleanNum1 = 0;
+            if (!double.TryParse(num1, out cleanNum1))            //Try parse converts string to a double and saves under clean new name
+            {                                                           //If a unvalid character will trigger warning
+                firstTextBox.Clear();
+                firstTextBox.Text = "Error: Invalid Char";
+            }
+
+            double cleanNum2 = 0;
+            if (!double.TryParse(num2, out cleanNum2))           //Will check if unvalid value and return warning
+            {
+                secondTextBox.Clear();
+                secondTextBox.Text = "Error: Invalid Char";
+            }
 
             double temp = cleanNum1 + cleanNum2;
             textAnswer.Text = Convert.ToString(temp);
-        }
-
-        public double debugFirstBox(string number)
-        {
-            //Goal: Return a double that has been error checked and converted from string
-
-            double cleanNumber = 0;
-            if (!double.TryParse(number, out cleanNumber))                     //Try parse converts string to a double and saves under clean new name
-            {                                                                  //If a unvalid character will trigger warning
-                firstTextBox.Clear();
-                firstTextBox.Text = "Invalid Character: Try again";
-            }
-            return cleanNumber;
-        }
-        public double debugSecondBox(string number)
-        {
-            //Goal: Return a double that has been error checked and converted from string
-
-            double cleanNumber = 0;
-            if (!double.TryParse(number, out cleanNumber))                     //Try parse converts string to a double and saves under clean new name
-            {                                                                  //If a unvalid character will trigger warning
-                secondTextBox.Clear();
-                secondTextBox.Text = "Invalid Character: Try again";
-            }
-            while (cleanNumber == 0)                                         //Check if we are trying to divide by zero
-            {                                                           //Returns an error if trying to divide by zero
-                secondTextBox.Clear();
-                secondTextBox.Text = "Cannot Divide by 0: Try again";
-            }
-            return cleanNumber;
         }
     }
 }
